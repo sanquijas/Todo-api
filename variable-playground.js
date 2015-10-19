6e29e08baa5790680d99baa84272415d506c685d
Ivan Sanchez <sanquijas@gmail.com>
2015-10-19 09:44:50 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,31 @@
+//var person = {
+//  name: 'Andrew',
+//  age: 21
+//}
+
+//function updatePerson(obj) {
+  //if (obj.age) {
+  //  obj.age= 32;
+  //}
+  //console.log(obj);
+
+  //obj = {
+  //  name: 'Andrew',
+  //  age: 24
+  //}
+  //console.log(obj);
+//}
+
+//updatePerson(person);
+//console.log(person);
+
+// Array Example
+var grades =  [80,90];
+
+function addGrade(obj) {
+  obj.push(60)
+  //obj = [80,90,60]
+}
+
+addGrade(grades);
+console.log(grades);
