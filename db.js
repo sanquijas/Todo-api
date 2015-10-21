8b6b0906b804d303464917c42ebd6ab59c4cdd02
Ivan Sanchez <sanquijas@gmail.com>
2015-10-21 14:51:19 -0700

    Fixed bug in db.js

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env === 'production') {
+if (env === 'production') {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    'dialect': 'postgres'
   });
 } else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
@@ -20,4 +20,3 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
-
