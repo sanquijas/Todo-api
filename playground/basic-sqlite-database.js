6628b9d2d66e0ec3748699f17df9aaed57720e06
Ivan Sanchez <sanquijas@gmail.com>
2015-10-26 21:13:29 -0700

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,45 +19,40 @@ var Todo = sequelize.define('todo', {
   }
 });
 
+var User = sequelize.define('user', {
+  email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
 sequelize.sync({
   //force: true
 }).then(function () { 
   console.log('Everything is synced');
 
-  Todo.findById(3).then(function (todo) {
-    if (todo) {
-      console.log(todo.toJSON());
-    } else {
-      console.log('Todo not found');
-    }
+  User.findById(1).then(function (user) {
+    user.getTodos({
+      where: {
+        completed: !true
+      }
+    }).then(function (todos) {
+        todos.forEach(function (todo) {
+          console.log(todo.toJSON());
+        })
+    });
+
   });
 
-/*
-  Todo.create({
-    description: "Take out trash"
-  }).then(function (todo) {
-      return Todo.create({
-        description: 'Clean office'
-      });
-  }).then(function() {
-      //return Todo.findById(1)
-      return Todo.findAll({
-        where: {
-          description: {
-            $like: '%office%'
-          }
-        }
-      })
-  }).then(function(todos) {
-    if (todos) {
-      todos.forEach(function (todo) {
-          console.log(todo.toJSON());
-      });
-    } else {
-        console.log('no todo found!');
-    }
-  }).catch(function (e) {
-      console.log(e);
-  });
-*/
+  //User.create({
+  //  email: 'ivan@example.com'
+  //}).then(function() {
+  //    return Todo.create({
+  //      description: 'Clean yard'
+  //    });
+  //}).then(function (todo) {
+  //    User.findById(1).then(function (user) {
+  //      user.addTodo(todo);
+  //    });
+  //});
 });
